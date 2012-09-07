e25ecab05cd9db9d918834ebc6c80f4c58d162bf
Daniel Nephin <dnephin@gmail.com>
2012-09-06 21:00:53 -0700

    Fix some tabs to spaces.

--- flatvault/crypt.py
+++ flatvault/crypt.py
@@ -12,26 +12,26 @@ BLOCK = Blowfish.block_size
 
 
 def get_padding(source_length, block_size=BLOCK):
-    bytes     	= block_size - source_length % block_size
+    bytes = block_size - source_length % block_size
     return pack('b' * bytes, *range(bytes))
 
 
 def strip_padding(source):
-	return source.rsplit(chr(0), 1)[0]
+    return source.rsplit(chr(0), 1)[0]
 
 
 def encrypt(source, key):
     assert 4 <= len(key) <= 56
-    iv                 	= Random.new().read(BLOCK)
-    cipher             	= Blowfish.new(key, MODE, iv)
-    source_length		= len(source)
-    padding            	= get_padding(source_length)
+    iv                  = Random.new().read(BLOCK)
+    cipher              = Blowfish.new(key, MODE, iv)
+    source_length       = len(source)
+    padding             = get_padding(source_length)
     return iv + cipher.encrypt(source + padding)
 
 
 def decrypt(source, key):
     assert 4 <= len(key) <= 56
-    bs 					= BLOCK
-    iv, source         	= source[:bs], source[bs:]
-    cipher             	= Blowfish.new(key, MODE, iv)
+    bs                  = BLOCK
+    iv, source          = source[:bs], source[bs:]
+    cipher              = Blowfish.new(key, MODE, iv)
     return strip_padding(cipher.decrypt(source))
